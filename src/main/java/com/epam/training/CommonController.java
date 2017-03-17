896a6c1b8813032de6b1d049c52fa20a3718e4a9
NatalliaKhadunai <hodunay2009@yandex.ru>
2017-03-17 18:08:48 +0300

    File download added

--- src/main/java/com/epam/training/CommonController.java
+++ src/main/java/com/epam/training/CommonController.java
@@ -4,13 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CommonController {
+    private static final int BUFFER_SIZE = 4096;
 
     @RequestMapping("/")
     public String getIndexPage() {
@@ -34,5 +41,47 @@ public class CommonController {
         if (chosenFile.isDirectory())
             for (File file : chosenFile.listFiles()) fileList.add(file);
         return fileList;
+    }
+
+    @RequestMapping("/downloadFile")
+    @ResponseBody
+    public void todo(HttpServletRequest request,
+                     HttpServletResponse response,
+                     @RequestParam String path) throws IOException {
+        ServletContext context = request.getServletContext();
+        File downloadFile = new File(path);
+        FileInputStream inputStream = new FileInputStream(downloadFile);
+
+        // get MIME type of the file
+        String mimeType = context.getMimeType(path);
+        if (mimeType == null) {
+            // set to binary type if MIME mapping not found
+            mimeType = "application/octet-stream";
+        }
+        System.out.println("MIME type: " + mimeType);
+
+        // set content attributes for the response
+        response.setContentType(mimeType);
+        response.setContentLength((int) downloadFile.length());
+
+        // set headers for the response
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"",
+                downloadFile.getName());
+        response.setHeader(headerKey, headerValue);
+
+        // get output stream of the response
+        OutputStream outStream = response.getOutputStream();
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead = -1;
+
+        // write bytes read from the input stream into the output stream
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+
+        inputStream.close();
+        outStream.close();
     }
 }
